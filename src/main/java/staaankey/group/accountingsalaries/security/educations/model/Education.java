523b293faf4a9043514c5aa37f836c0cdd679c81
staaankey <dimaclopov@gmail.com>
2023-10-09 14:18:50 +0300

    Educations feature for employee add

--- src/main/java/staaankey/group/accountingsalaries/security/educations/model/Education.java
+++ src/main/java/staaankey/group/accountingsalaries/security/educations/model/Education.java
@@ -1,4 +1,4 @@
-package staaankey.group.accountingsalaries.educations.model;
+package staaankey.group.accountingsalaries.security.educations.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
